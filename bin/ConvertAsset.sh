26133c6588eb1270ac7132e13faa41eb6f4c792c
lixin <li.xin@pset.suntec.net>
2015-09-06 14:13:08 +0800

    update
    Redmine/Any: http://iredmine/issues/130984
    Change-Id: I29c3511ee78a5fd53d4deb87836678f653feae79

--- bin/ConvertAsset.sh
+++ bin/ConvertAsset.sh
@@ -11,3 +11,9 @@ tool/AssetImporter model Asset/Stand_To_Walk_Left_90.FBX Data/Animation/Stand_To
 tool/AssetImporter model Asset/Stand_To_Walk_Left_180.FBX Data/Animation/Stand_To_Walk_Left_180.mdl -nomodel -nm -nt -motion rz -flipbone
 tool/AssetImporter model Asset/Stand_To_Walk_Right_90.FBX Data/Animation/Stand_To_Walk_Right_90.mdl -nomodel -nm -nt -motion rx -flipbone
 tool/AssetImporter model Asset/Stand_To_Walk_Right_180.FBX Data/Animation/Stand_To_Walk_Right_180.mdl -nomodel -nm -nt -motion rz -flipbone
+
+tool/AssetImporter model Asset/Attack_Close_Forward_07.FBX Data/Animation/Attack_Close_Forward_07.mdl -nomodel -nm -nt -motion z -flipbone
+tool/AssetImporter model Asset/Attack_Close_Forward_08.FBX Data/Animation/Attack_Close_Forward_08.mdl -nomodel -nm -nt -motion z -flipbone
+
+tool/AssetImporter model Asset/Counter_Arm_Front_01.FBX Data/Animation/Counter_Arm_Front_01.mdl -nomodel -nm -nt -origin xz -motion z -flipbone
+tool/AssetImporter model Asset/Counter_Arm_Front_01_TG.FBX Data/Animation/Counter_Arm_Front_01_TG.mdl -nomodel -nm -nt -origin xzr -motion z -flipbone
