5919a5b3ddacdf31d7709af98a28c25481cc545b
lixin <li.xin@pset.suntec.net>
2015-09-14 11:45:35 +0800

    add batch process script
    Redmine/Any: http://iredmine/issues/130984
    Change-Id: I9953093efe113ba1f6c982207505d585067dc8e4

--- bin/ConvertAsset.sh
+++ bin/ConvertAsset.sh
@@ -1,27 +1,3 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-mkdir MyData/Models
-mkdir MyData/Animation
-
-tool/AssetImporter model Asset/bruce.FBX MyData/Models/bruce.mdl -t -na -l -cm -ct -flipbone
-
-tool/AssetImporter model Asset/Stand_Idle.FBX MyData/Animation/Stand_Idle.mdl -nomodel -nm -nt -flipbone
-tool/AssetImporter model Asset/Stand_Idle_01.FBX MyData/Animation/Stand_Idle_01.mdl -nomodel -nm -nt -flipbone
-tool/AssetImporter model Asset/Stand_Idle_02.FBX MyData/Animation/Stand_Idle_02.mdl -nomodel -nm -nt -flipbone
-
-tool/AssetImporter model Asset/Walk_Forward.FBX MyData/Animation/Walk_Forward.mdl -nomodel -nm -nt -motion z -flipbone
-
-tool/AssetImporter model Asset/Turn_Left_90.FBX MyData/Animation/Turn_Left_90.mdl -nomodel -nm -nt -motion r -flipbone
-tool/AssetImporter model Asset/Turn_Left_180.FBX MyData/Animation/Turn_Left_180.mdl -nomodel -nm -nt -motion r -flipbone
-tool/AssetImporter model Asset/Turn_Right_90.FBX MyData/Animation/Turn_Right_90.mdl -nomodel -nm -nt -motion r -flipbone
-tool/AssetImporter model Asset/Turn_Right_180.FBX MyData/Animation/Turn_Right_180.mdl -nomodel -nm -nt -motion r -flipbone
-
-tool/AssetImporter model Asset/Attack_Close_Forward_07.FBX MyData/Animation/Attack_Close_Forward_07.mdl -nomodel -nm -nt -motion z -flipbone
-tool/AssetImporter model Asset/Attack_Close_Forward_08.FBX MyData/Animation/Attack_Close_Forward_08.mdl -nomodel -nm -nt -motion z -flipbone
-tool/AssetImporter model Asset/Attack_Close_Left.FBX MyData/Animation/Attack_Close_Left.mdl -nomodel -nm -nt -motion rx -flipbone
-
-tool/AssetImporter model Asset/Counter_Arm_Front_01.FBX MyData/Animation/Counter_Arm_Front_01.mdl -nomodel -nm -nt -origin xz -motion xz -flipbone
-tool/AssetImporter model Asset/Counter_Arm_Front_01_TG.FBX MyData/Animation/Counter_Arm_Front_01_TG.mdl -nomodel -nm -nt -origin xzr -motion xz -flipbone
-
-tool/AssetImporter model Asset/Evade_Forward_01.FBX MyData/Animation/Evade_Forward_01.mdl -nomodel -nm -nt -motion xz -flipbone
-tool/AssetImporter model Asset/Evade_Back_01.FBX MyData/Animation/Evade_Back_01.mdl -nomodel -nm -nt -motion rxz -flipbone
+$(dirname $0)/Urho3DPlayer Scripts/BatchProcessAsset.as -p 'CoreData;Data;MyData' -headless
---
+++ bin/MyData/Scripts/BatchProcessAsset.as
@@ -0,0 +1,67 @@
+
+const String OUT_DIR = "MyData/";
+const String ASSET_DIR = "Asset/";
+const String MODEL_ARGS = " -t na -l -cm -ct";
+const String ANIMATION_ARGS = " -nm -nt";
+const bool showOutput = false;
+
+void ExecuteCmd(const String&in cmd)
+{
+    Print("[CMD] " + cmd);
+    fileSystem.SystemCommand(cmd, showOutput);
+}
+
+void PreProcess()
+{
+    fileSystem.CreateDir(OUT_DIR + "Models");
+    fileSystem.CreateDir(OUT_DIR + "Animations");
+}
+
+String DoProcess(const String&in name, const String&in folderName, const String&in args)
+{
+    String iname = folderName + name;
+    uint pos = name.FindLast('.');
+    String oname = OUT_DIR + folderName + name.Substring(0, pos) + ".mdl";
+    pos = oname.FindLast('/');
+    String outFolder = oname.Substring(0, pos);
+    fileSystem.CreateDir(outFolder);
+    ExecuteCmd("tool/AssetImporter model Asset/" + iname + " " + oname + args);
+    return oname;
+}
+
+void ProcessModels()
+{
+    Array<String> models = fileSystem.ScanDir(ASSET_DIR + "Models", "*.*", SCAN_FILES, true);
+    for (uint i=0; i<models.length; ++i)
+    {
+        Print("Found a model " + models[i]);
+        DoProcess(models[i], "Models/", MODEL_ARGS);
+    }
+}
+
+void ProcessAnimations()
+{
+    Array<String> animations = fileSystem.ScanDir(ASSET_DIR + "Animations", "*.*", SCAN_FILES, true);
+    for (uint i=0; i<animations.length; ++i)
+    {
+        Print("Found a animation " + animations[i]);
+        String outMdlName = DoProcess(animations[i], "Animations/", ANIMATION_ARGS);
+        fileSystem.Delete(outMdlName);
+    }
+}
+
+void PostProcess()
+{
+    fileSystem.SystemCommand("rm -rf MyData/Animations/*.mdl", showOutput);
+}
+
+void Start()
+{
+    uint startTime = time.systemTime;
+    PreProcess();
+    ProcessModels();
+    ProcessAnimations();
+    PostProcess();
+    Print("\n\n BATCH PROCESS TIME COST = " + String(time.systemTime - startTime) + " ms");
+    engine.Exit();
+}
