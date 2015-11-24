15b6ee0c597db0c7422e4eee2fbc863cd941c2dd
299299 <magic.lixin@gmail.com>
2015-11-24 14:34:28 +0800

    update

--- bin/make_release.bat
+++ bin/make_release.bat
@@ -1,15 +1,16 @@
 RMDIR /S /Q .\Release\
-md Release\
-xcopy /s CoreData\ Release\CoreData\
-xcopy /s Data\ Release\Data\
-xcopy /s MyData\ Release\MyData\
-cp Test.bat Release\Game.bat
-cp Test.sh Release\Game.sh
-tool\ScriptCompiler.exe Release\MyData\Scripts\Test.as
-del Release\MyData\Scripts\*.as
-tool\PackageTool Release\CoreData Release\CoreData.pak -c -q
-tool\PackageTool Release\Data Release\Data.pak -c -q
-tool\PackageTool Release\MyData Release\MyData.pak -c -q
-RMDIR /S /Q Release\CoreData
-RMDIR /S /Q Release\Data
-RMDIR /S /Q Release\MyData
+md .\Release\
+xcopy /s /e /h .\CoreData .\Release\CoreData\
+xcopy /s /e /h .\Data .\Release\Data\
+xcopy /s /e /h .\MyData .\Release\MyData\
+copy .\Test.bat .\Release\Game.bat
+copy /f .\Test.sh .\Release\Game.sh
+copy /f .\Urho3DPlayer.exe .\Release\
+.\tool\ScriptCompiler.exe .\Release\MyData\Scripts\Test.as
+del .\Release\MyData\Scripts\*.as
+.\tool\PackageTool .\Release\CoreData .\Release\CoreData.pak -c -q
+.\tool\PackageTool .\Release\Data .\Release\Data.pak -c -q
+.\tool\PackageTool .\Release\MyData .\Release\MyData.pak -c -q
+RMDIR /S /Q .\Release\CoreData
+RMDIR /S /Q .\Release\Data
+RMDIR /S /Q .\Release\MyData
