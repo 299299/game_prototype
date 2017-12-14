24fa6a9dff497179435990485203d6da776b79a4
Golden Li <Golden.Li@bmw.com>
2017-12-14 21:07:05 +0800

    update

--- bin/sync_android_assets.sh
+++ bin/sync_android_assets.sh
@@ -4,4 +4,6 @@ rm -rf ~/StudioProjects/build_android/app/src/main/assets/MyData/;
 cp -rf MyData ~/StudioProjects/build_android/app/src/main/assets/
 
 rm -rf ~/StudioProjects/build_android/app/src/main/assets/Data/;
-cp -rf Data ~/StudioProjects/build_android/app/src/main/assets/
+cp -rf TargetData ~/StudioProjects/build_android/app/src/main/assets/Data
+
+rm -rf ~/StudioProjects/build_android/app/src/main/assets/Autoload/;
